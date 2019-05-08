99d1f634100d60407a85b1ba5041e6e83f03aede
Leandro Pereira <lcpereir@MacBook-Pro-de-Leandro.local>
2019-05-08 00:23:53 -0300

    clean target

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
--- insert some POIs into the database
-insert into poi values(10001,'Lanchonete', 27, 12);
-insert into poi values(10002,'Posto', 31, 18);
-insert into poi values(10003,'Joalheria', 15, 12);
-insert into poi values(10004,'Floricultura', 19, 21);
-insert into poi values(10005,'Pub', 12, 8);
-insert into poi values(10006,'Supermercado', 23, 6);
-insert into poi values(10007,'Churrascaria', 28, 2);
